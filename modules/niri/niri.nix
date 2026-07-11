a2d21b7b8890214862eb0e1887324c67f4682dca
zakme <zakmexz@gmail.com>
2026-07-11 23:07:17 +0100

    .

--- modules/niri/niri.nix
+++ modules/niri/niri.nix
@@ -1,7 +1,7 @@
 { config, lib, pkgs, ... }:
 
 {
-    xdg.configFile."niri/config.kdl".source = config.lib.file/mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/modules/niri/config.kdl";
+    xdg.configFile."niri/config.kdl".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/modules/niri/config.kdl";
     xdg.configFile."niri/config.kdl".force = true;
 }
 
