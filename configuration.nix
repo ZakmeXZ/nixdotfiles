2ef5383d2fb5c3f5d07d1128aa2dece508130c8d
zakme <zakmexz@gmail.com>
2026-07-12 18:51:42 +0100

    Add latest nvidia drivers

--- configuration.nix
+++ configuration.nix
@@ -94,7 +94,7 @@
 		intelBusId = "PCI:0:2:0";
 		nvidiaBusId = "PCI:1:0:0";
 	};	
-	package = config.boot.kernelPackages.nvidiaPackages.stable;
+	package = config.boot.kernelPackages.nvidiaPackages.latest;
   };
 
   environment.sessionVariables = {
