aadb762eca62a491969f343a7ac5348f500591db
zakme <zakmexz@gmail.com>
2026-07-12 16:29:21 +0100

    .

--- configuration.nix
+++ configuration.nix
@@ -117,6 +117,9 @@
 	enable = true;
 	remotePlay.openFirewall = true;
 	dedicatedServer.openFirewall = true;
+    extraCompatPackages = with pkgs; [
+        proton-ge-bin
+    ];
   };
 
   programs.neovim = {
