ae2d860b6bca1f958b9fbf51a02f9480b5abc3ec
zakme <zakmexz@gmail.com>
2026-07-13 01:17:20 +0100

    .

--- stylix.nix
+++ stylix.nix
@@ -4,6 +4,27 @@
     stylix = {
         enable = true;
         image = ./wallpapers/wallpaper.png;
+        base16Scheme = "${pkgs.base16-schemes}/share/themes/everforest.yaml";
         polarity = "dark";
+        fonts = {
+            monospace = {
+                package = pkgs.nerd-fonts.jetbrains-mono;
+                name = "JetBrainsMono Nerd Font";
+            };
+            sansSerif = {
+                package = pkgs.inter;
+                name = "Inter";
+            };
+            serif = {
+                package = pkgs.inter;
+                name = "Inter";
+            };
+            sizes = {
+                applications = 11;
+                terminal = 12;
+                desktop = 11;
+            };
+        };
+        targets.ghostty.enable = false;
     };
 }
